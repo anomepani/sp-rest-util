528042be25ff1d3b777af9172429d76523dd7ec9
Arvind Mepani <arvindmepani@gmail.com>
2020-02-02 20:20:05 +0530

    Added Support for SPMultiFileUpload method

--- SPOHelper.ts
+++ SPOHelper.ts
@@ -68,7 +68,7 @@ const _Post = ({ url, payload = {}, hdrs = {}, isBlobOrArrayBuffer = false }) =>
 /**
   * Get Request Digest
   */
-export const GetDigest = (url = "") => {
+ export const GetDigest = (url = "") => {
     //// TODO -Cached based Request Digest
 
     //let headers = Object.assign({},_headers)// JPS(JSFY(_headers));
@@ -93,7 +93,7 @@ export const GetDigest = (url = "") => {
  * Get 
  * Examples SPGet("https://tenant.sharepoint.com/sites/ABCSite/_api/Lists/getbytitle('SPO List')").then(r=>console.log(r))
  */
-export const SPGet = url => {
+ export const SPGet = url => {
     let headers = copyObj(_headers);
     return GetJson(url, { headers }); //BaseClient(url, { headers }).then(r => r.json());
 };
@@ -103,7 +103,7 @@ export const SPGet = url => {
   * SPPost({url:"https://tenant.sharepoint.com/sites/ABCSite/_api/Lists",payload:{Title :"POC Doc", BaseTemplate: 101,Description: 'Created From SPOHelper' }}).then(r=>console.log(r))
    * Example  : SPPost({url:"https://tenant.sharepoint.com/sites/ABCSite/_api/Lists/getbytitle('SPO List')/items",payload:{Title :"POST test",Number:123}}).then(r=>console.log(r))
   */
-export const SPPost = async ({ url, payload = {}, hdrs = {}, digest = defaultDigest, isBlobOrArrayBuffer = false }) => {
+ export const SPPost = async ({ url, payload = {}, hdrs = {}, digest = defaultDigest, isBlobOrArrayBuffer = false }) => {
     //If Digest is null or undefined then request for new digest otherwise pass digest as it is
     ///NEED TO TEST Below logic
 
@@ -118,27 +118,44 @@ export const SPPost = async ({ url, payload = {}, hdrs = {}, digest = defaultDig
   * Add attachment using SPFileUpload metod with payload Blob type
   * SPFileUpload({url:"https://tenant.sharepoint.com/_api/Lists/GetByTitle('SPOList')/items(1)//AttachmentFiles/ add(FileName='abc3.txt') ",payload:new Blob(["This is text"],{type:"text/plain"})}).then(r=>console.log(r))
   */
-export const SPFileUpload = async ({ url, payload = {}, hdrs = {}, digest = defaultDigest }) => {
+ export const SPFileUpload = async ({ url, payload = {}, hdrs = {}, digest = defaultDigest }) => {
     return SPPost({ isBlobOrArrayBuffer: true, url, hdrs, digest, payload });
 };
-export const SPMultiFileUpload =  ({ url, payload = {}, hdrs = {}, files = [], digest = defaultDigest }) => {
 
-    var promiseAll = files.map(async (i) => {
+ export const SPMultiFileUpload =  ({ url, payload = {}, hdrs = {}, files = [], digest = defaultDigest }) => {
+var promiseAll =[];
+//Wait Till all file uploading.
+return new Promise(async(resolve,reject)=>{
+
+
+  //  files.forEach(async (i,v) =>
+//console.log("Processing iles",files.length);
+for(var j=0;j<files.length;j++)
+ {
+var i=files[j]
+//console.log("Processing",j,i);
+//Uploading file one by one in loop 
         //TODO Encode/decode filename
-        return await SPFileUpload(
+        var abc= await SPFileUpload(
             {
                 url: `${url}add(FileName='${i.fileName}')`
                 , payload: i.data, digest
             });
-    });
 
-    return promiseAll;
+
+promiseAll.push(abc);
+if(promiseAll.length==files.length)
+resolve(promiseAll);
+    };
+});
 };
+
+
 /**
   * Update Request with Request Digest
    * Example  : SPUpdate({url:"https://tenant.sharepoint.com/sites/ABCSite/_api/Lists/getbytitle('SPO List')/items(1)",payload:{Title :"POST test update",Number:1234}}).then(r=>console.log(r))
   */
-export const SPUpdate = ({ url, payload = {}, digest = defaultDigest }) => {
+ export const SPUpdate = ({ url, payload = {}, digest = defaultDigest }) => {
     let hdrs = {
         "IF-MATCH": "*",
         "X-HTTP-Method": "MERGE"
@@ -149,10 +166,20 @@ export const SPUpdate = ({ url, payload = {}, digest = defaultDigest }) => {
   * Delete Request with Request Digest
   * Example  : SPDelete("https://tenant.sharepoint.com/sites/ABCSite/_api/Lists/getbytitle('SPO List')/items(3)").then(r=>console.log(r))
   */
-export const SPDelete = (url, digest = defaultDigest) => {
+ export const SPDelete = (url, digest = defaultDigest) => {
     let hdrs = {
         "IF-MATCH": "*",
         "X-HTTP-Method": "DELETE"
     };
     return SPPost({ url, hdrs, digest });
 };
+
+let GetUniqueFileData=()=>{return new Blob([GetUniqueFileName()])}
+
+let GetUniqueFileName=()=>new Date().getTime() +".txt";
+
+let GenerateSampleFileArray=(count)=>{
+return Array.from(Array(count)).map((_, i) => { 
+//console.log(_,i)
+var fileName=GetUniqueFileName(),data=GetUniqueFileData();
+return {fileName,data} })};
